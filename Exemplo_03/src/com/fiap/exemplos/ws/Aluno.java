22bf568d007b62aa773b3759e9aeabcc16faa58a
Vagner Panarello Filho <RM30222@fiap.com.br>
2016-09-17 11:48:16 -0300

    cliente glasfish

---
+++ Exemplo_03/src/com/fiap/exemplos/ws/Aluno.java
@@ -0,0 +1,26 @@
+package com.fiap.exemplos.ws;
+
+public class Aluno {
+	private String nome;
+	private String turma;
+	private String media;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTurma() {
+		return turma;
+	}
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
+	public String getMedia() {
+		return media;
+	}
+	public void setMedia(String media) {
+		this.media = media;
+	}
+
+}
