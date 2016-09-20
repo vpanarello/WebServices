c6d8a7f630320f269ae367a68fd5219da35de3d1
Vagner Panarello Filho <RM30222@fiap.com.br>
2016-09-19 22:08:06 -0300

    ClientJersey

---
+++ ClientJersey/src/main/java/br/com/fiap/client/Alunos.java
@@ -0,0 +1,27 @@
+package br.com.fiap.client;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="alunoes")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Alunos {
+	
+	@XmlElement(name= "aluno")
+	private List<Aluno> alunos;
+
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+	
+	
+
+}
---
+++ ClientJersey/src/main/java/br/com/fiap/client/Retorno.java
@@ -0,0 +1,22 @@
+package br.com.fiap.client;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="retorno")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Retorno {
+	
+	private String mensagem;
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+
+}
